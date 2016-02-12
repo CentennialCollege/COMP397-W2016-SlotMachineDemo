7fdfacf2c4eb3da3094094e82d284baccb3bd4f7
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-02-12 18:25:45 -0500

    added reels function

--- Scripts/scenes/slotmachine.js
+++ Scripts/scenes/slotmachine.js
@@ -11,6 +11,14 @@ var scenes;
         // CONSTRUCTOR ++++++++++++++++++++++
         function SlotMachine() {
             _super.call(this);
+            this._grapes = 0;
+            this._bananas = 0;
+            this._oranges = 0;
+            this._cherries = 0;
+            this._bars = 0;
+            this._bells = 0;
+            this._sevens = 0;
+            this._blanks = 0;
         }
         // PUBLIC METHODS +++++++++++++++++++++
         // Start Method
@@ -40,6 +48,55 @@ var scenes;
         // SLOT_MACHINE Scene updates here
         SlotMachine.prototype.update = function () {
         };
+        //PRIVATE METHODS
+        /* Utility function to check if a value falls within a range of bounds */
+        SlotMachine.prototype._checkRange = function (value, lowerBounds, upperBounds) {
+            return (value >= lowerBounds && value <= upperBounds) ? value : -1;
+        };
+        /* When this function is called it determines the betLine results.
+        e.g. Bar - Orange - Banana */
+        SlotMachine.prototype._reels = function () {
+            var betLine = [" ", " ", " "];
+            var outCome = [0, 0, 0];
+            for (var spin = 0; spin < 3; spin++) {
+                outCome[spin] = Math.floor((Math.random() * 65) + 1);
+                switch (outCome[spin]) {
+                    case this._checkRange(outCome[spin], 1, 27):
+                        betLine[spin] = "blank";
+                        this._blanks++;
+                        break;
+                    case this._checkRange(outCome[spin], 28, 37):
+                        betLine[spin] = "Grapes";
+                        this._grapes++;
+                        break;
+                    case this._checkRange(outCome[spin], 38, 46):
+                        betLine[spin] = "Banana";
+                        this._bananas++;
+                        break;
+                    case this._checkRange(outCome[spin], 47, 54):
+                        betLine[spin] = "Orange";
+                        this._oranges++;
+                        break;
+                    case this._checkRange(outCome[spin], 55, 59):
+                        betLine[spin] = "Cherry";
+                        this._cherries++;
+                        break;
+                    case this._checkRange(outCome[spin], 60, 62):
+                        betLine[spin] = "Bar";
+                        this._bars++;
+                        break;
+                    case this._checkRange(outCome[spin], 63, 64):
+                        betLine[spin] = "Bell";
+                        this._bells++;
+                        break;
+                    case this._checkRange(outCome[spin], 65, 65):
+                        betLine[spin] = "Seven";
+                        this._sevens++;
+                        break;
+                }
+            }
+            return betLine;
+        };
         //EVENT HANDLERS ++++++++++++++++++++
         SlotMachine.prototype._bet1ButtonClick = function (event) {
             console.log("Bet 1 Credit");
@@ -52,6 +109,7 @@ var scenes;
         };
         SlotMachine.prototype._spinButtonClick = function (event) {
             console.log("Spin those reels!");
+            console.log(this._reels());
         };
         return SlotMachine;
     })(objects.Scene);
