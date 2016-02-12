07c5db439b79e79604b9d8494947427370b523fb
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-02-12 17:00:56 -0500

    added slotmachine image to game scene

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -18,7 +18,8 @@ var assetData:objects.Asset[] = [
     {id: "BackButton", src:"../../Assets/images/BackButton.png"},
     {id: "Nextbutton", src:"../../Assets/images/Nextbutton.png"},
     {id: "StartButton", src:"../../Assets/images/StartButton.png"},
-    {id: "StartOverButton", src:"../../Assets/images/StartOverButton.png"}
+    {id: "StartOverButton", src:"../../Assets/images/StartOverButton.png"},
+    {id: "SlotMachine", src:"../../Assets/images/SlotMachine.png"}
 ];
 
 function preload() {
--- Scripts/scenes/slotmachine.ts
+++ Scripts/scenes/slotmachine.ts
@@ -2,8 +2,7 @@
 module scenes {
     export class SlotMachine extends objects.Scene {
         //PRIVATE INSTANCE VARIABLES ++++++++++++
-        private _slotMachineLabel:objects.Label;
-        
+        private _backgroundImage: createjs.Bitmap;
         // CONSTRUCTOR ++++++++++++++++++++++
         constructor() {
             super();
@@ -13,15 +12,10 @@ module scenes {
         
         // Start Method
         public start(): void {    
-            
-            // add the SLOT MACHINE Label to the MENU scene
-            this._slotMachineLabel = new objects.Label(
-                "GAME GOES HERE", 
-                "60px Consolas", 
-                "#000000", 
-                config.Screen.CENTER_X, 
-                config.Screen.CENTER_Y);
-            this.addChild(this._slotMachineLabel);          
+            // add background image to the scene
+            this._backgroundImage = new createjs.Bitmap(assets.getResult("SlotMachine"));
+            this.addChild(this._backgroundImage);
+                   
             
             // add this scene to the global stage container
             stage.addChild(this);
